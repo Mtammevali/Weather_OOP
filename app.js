26535b6e553d43134644120572056cfa781f05be
Marek Tammevali <marek.tammevali@khk.ee>
2021-12-02 11:09:03 +0200

    UI added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //weather objct
 const weather = new Weather( 'Tartu');
 
+const ui = new  UI();
 //get city weather
 function getWeather(){
     weather.getWeather()
         .then(data =>{
             console.log(data)
+            ui.drawWeather(data)
         })
         .catch(error => console.log(error))
 }
