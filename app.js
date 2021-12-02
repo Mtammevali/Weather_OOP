138c22b5beca3d75162032cf13b8ed6e1ac40029
Marek Tammevali <marek.tammevali@khk.ee>
2021-12-02 11:43:56 +0200

    get city set city LS

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-//weather objct
-const weather = new Weather( 'Tartu');
+//LS Objct
+const ls = new LS()
+//city data
+const initCity = ls.getCity()
 
+
+//weather objct
+const weather = new Weather( initCity);
+//ui objct
 const ui = new  UI();
+
+//events
+const form = document.querySelector('#changeCity')
+form.addEventListener('submit', changeWeather)
+
+
+//change weather
+function changeWeather(event){
+    const city = document.querySelector('#cityName').value;
+    weather.changeCity(city)
+    ls.setCity(city)
+    getWeather()
+    document.querySelector('#cityName').value =''
+    event.preventDefault()
+}
+
+
 //get city weather
 function getWeather(){
     weather.getWeather()
